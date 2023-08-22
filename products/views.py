fceb1b0d30a5e3c2e7210279410c9c1286dcceab
andrei0075 <114240378+andrei0075@users.noreply.github.com>
2023-08-22 19:02:20 +0200

    добовление json

--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from products.models import ProductCategory, Product
+
 # Create your views here.
+
+def index(request):
+    сontext = {'title': 'Test Title',
+               'username': 'andrei',
+               }
+
+    return render(request, 'products/index.html', сontext)
+
+
+def products(request):
+    context = {
+        'title':'Store - Каталог',
+        'products': Product.objects.all(),
+        'categories':ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
